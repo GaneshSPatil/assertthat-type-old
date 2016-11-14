c25f055be12b2660eb4aa7a9635561d8e53a4e3f
GaneshSPatil <ganeshpl@thoughtworks.com>
2016-11-14 08:34:29 +0530

    Added more assertion methods for typecheck

--- src/assertthat-plugin.js
+++ src/assertthat-plugin.js
@@ -12,8 +12,51 @@ module.exports = function() {
         if(check.number(actual)) {
           return;
         }
-        assert.fail(undefined, undefined, 'Expected ' + actual + ' to be a Number.');
+        assert.fail(undefined, undefined, 'Expected `' + actual + '` to be a Number.');
     };
+
+    is.string = function() {
+      if(check.string(actual)) {
+        return;
+      }
+      assert.fail(undefined, undefined, 'Expected `' + actual + '` to be a String.');
+    };
+
+    is.boolean = function() {
+      if(check.boolean(actual)) {
+        return;
+      }
+      assert.fail(undefined, undefined, 'Expected `' + actual + '` to be boolean.');
+    };
+
+    is.object = function() {
+      if(check.object(actual)) {
+        return;
+      }
+      assert.fail(undefined, undefined, 'Expected `' + actual + '` to be an Object.');
+    };
+
+    is.array = function() {
+      if(check.object(actual)) {
+        return;
+      }
+      assert.fail(undefined, undefined, 'Expected `' + actual + '` to be an Array.');
+    };
+
+    is.date = function() {
+      if(check.date(actual)) {
+        return;
+      }
+      assert.fail(undefined, undefined, 'Expected `' + actual + '` to be a Date.');
+    };
+
+    is.function = function() {
+      if(check.function(actual)) {
+        return;
+      }
+      assert.fail(undefined, undefined, 'Expected `' + actual + '` to be a Function.');
+    };
+
   };
 
   return typePlugin;
